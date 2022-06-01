20422167575c265263331a95db39144070192f50
hxr404 <55095883+hxr404@users.noreply.github.com>
2022-06-02 01:28:00 +0200

    Replace rollup with Snowpack
    changed buildsystem because old one was not suited for the use-case.
    and fixed bug in package.json + package-lock.json

--- rollup.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-	input: 'main.js',
-	output: [
-		{
-			file: 'scripts.js',
-			format: 'iife'
-		}
-	]
-};
---
+++ snowpack.config.mjs
@@ -0,0 +1,9 @@
+// Example: snowpack.config.mjs
+// The added "@type" comment will enable TypeScript type information via VSCode, etc.
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+export default {
+  plugins: [
+    /* ... */
+  ],
+};
